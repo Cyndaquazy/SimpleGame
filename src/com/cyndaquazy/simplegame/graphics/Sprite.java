cab1af2d5973668ce19fb62487bed26b4b6872de
Cyndaquazy <cyndaquazy@gmail.com>
2014-06-20 16:43:17 -0400

    Updated GITIGNORE, Added Sprite and Main classes.

---
+++ src/com/cyndaquazy/simplegame/graphics/Sprite.java
@@ -0,0 +1,7 @@
+package com.cyndaquazy.simplegame.graphics;
+
+public class Sprite
+{
+  private int id;
+  private int textureLocation;
+}
---
+++ src/com/cyndaquazy/simplegame/main/Main.java
@@ -0,0 +1,11 @@
+package com.cyndaquazy.simplegame.main;
+
+public class Main
+{
+
+  public static void main(String[] args)
+  {
+    
+  }
+
+}
